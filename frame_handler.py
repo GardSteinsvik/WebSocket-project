a4864dd2ad33a416aa526504585983bce253c1b9
Gard Steinsvik <gardsteinsvik@gmail.com>
2017-04-26 16:50:19 +0200

    masking WIP

--- frame_handler.py
+++ frame_handler.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import struct
 
 OPCODE_CONTINUATION = 0x0
 OPCODE_TEXT = 0x1
@@ -8,22 +9,68 @@ OPCODE_PING = 0x9
 OPCODE_PONG = 0xa
 
 
+def mask(data):
+
+    payload = data.encode('utf-8')
+
+    message = ""
+
+    # Adding fin
+    b1 = '\x80'
+    # b1 |= OPCODE_TEXT
+    print(b1)
+    message += b1
+
+    b2 = 0
+    payload_length = len(payload)
+
+    if payload_length == 126:
+        b2 |= payload_length
+        print(chr(b2).encode('utf-8'))
+        message += chr(b2)
+    elif payload_length < ((2 ** 16) - 1):
+        b2 |= 126
+        message += chr(b2)
+        # packing to unsigned short
+        l = struct.pack(">H", payload_length)
+        message += l
+    else:
+        b2 |= 127
+        message += chr(b2)
+        # packing to unsigned long long
+        l = struct.pack(">Q", payload_length)
+        message += l
+
+    message += str(payload)
+
+    print(message.encode('utf-8'))
+
+    return message.encode('utf-8')
+
+
 def unmask(data):
     frame = bytearray(data)
 
     length = frame[1] & 127
     print('length: ' + str(length))
+
     mask_start = 2
+    if length == 126:
+        mask_start = 4
+    elif length == 127:
+        mask_start = 10
+
     data_start = mask_start + 4
 
     output_bytes = []
     for i in range(data_start, data_start+length):
         byte = frame[i] ^ frame[mask_start + (i - data_start) % 4]
-        print(byte)
         output_bytes.append(byte)
 
-    print(bytes(output_bytes).decode('windows-1252'))
+    return "".join(map(chr, output_bytes))
 
-data = {0x81, 0x83, 0xb4, 0xb5, 0x03, 0x2a, 0xdc, 0xd0, 0x6a}
+data = [0x81, 0x83, 0xb4, 0xb5, 0x03, 0x2a, 0xdc, 0xd0, 0x6a]
 
-unmask(data)
+print(mask('test'))
+
+
