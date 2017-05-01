abe054968e3c1fbaf88a69f7fece6efbc8b8f403
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2017-05-01 13:35:42 +0200

    single-threaded listening + non-working ssl

--- connection.py
+++ connection.py
@@ -1,19 +1,13 @@
 # -*- coding: utf-8 -*-
-from threading import Thread
-
 from web_socket import WebSocket
 
 
 class Connection:
-
     def __init__(self, conn):
         self.sock = WebSocket(conn)
 
-    def thread_handler(self):
+    def single_action(self):
         msg = '0'
         while msg:
             msg = self.sock.recv()
             print(msg)
-
-    def start(self):
-        Thread(target=self.thread_handler).start()
