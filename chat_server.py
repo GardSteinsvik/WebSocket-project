38abf850f2acde4d97c90df514f5725a7d87577c
Harald Floor Wilhelmsen <harald.floor.wilhelmsen@trondheim.kommune.no>
2017-05-09 14:18:22 +0200

    implemented disconnection handler in chat_server

--- chat_server.py
+++ chat_server.py
@@ -2,23 +2,13 @@
 import signal
 import sys
 
-import frame_handler
 from web_socket_server import WebSocketServer
 
 connections = []
 
 
 def message_handler(msg, connection):
-    """
-    This is the method that is called each time a message is received.
-    :param msg: The message that is received
-    :param connection: The connection that received the message
-    """
     for conn in connections:
-        if not conn.is_alive:
-            connections.remove(conn)
-            print('Removed dead conn')
-            continue
         if conn != connection:
             conn.send_text(msg)
 
@@ -26,6 +16,11 @@ def message_handler(msg, connection):
 def connection_handler(conn, addr):
     print('New connection from ' + str(addr))
     connections.append(conn)
+
+
+def disconnection_handler(conn):
+    print('Removed dead conn')
+    connections.remove(conn)
 
 
 def signal_handler(signal, frame):
@@ -40,6 +35,6 @@ def signal_handler(signal, frame):
 if __name__ == '__main__':
     s = WebSocketServer('0.0.0.0', 80)
     # starts the server. This call is non-blocking
-    s.start(message_handler, connection_handler, worker_thread_count=20)
+    s.start(message_handler, connection_handler, disconnection_handler, worker_thread_count=20)
 
     signal.signal(signal.SIGINT, signal_handler)
